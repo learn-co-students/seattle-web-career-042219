558d2924129213b3119a44376c521042f4263c23
John Franti <john.franti@flatironschool.com>
2019-06-03 13:34:53 -0700

    update notes

--- 30-dom-manipulation/john/js/scripts.js
+++ 30-dom-manipulation/john/js/scripts.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-     document.body.style.fontFamily = "comic sans ms";
+    //  document.body.style.fontFamily = "comic sans ms";
 
      document.querySelectorAll("li").forEach((item) => {
         let shortName = item.textContent.split(" ");
@@ -10,12 +10,13 @@ document.addEventListener('DOMContentLoaded', () => {
      })
 
      let newList = document.createElement("ul");
-     document.body.appendChild(newList);
+    document.querySelector(".div1").appendChild(newList);
+    // document.querySelector(".div4").appendChild(newList);
      newList.id = "kitties";
      setInterval(()=> {
          for (var i = 0; i < 3; i++) {
              let newCat = document.createElement("li");
-             newCat.textContent = "Meow";
+             newCat.textContent = "Meow  🐈";
              newCat.className = "mrWhiskers";
              document.querySelector("#kitties").appendChild(newCat);
         }
@@ -26,13 +27,14 @@ document.addEventListener('DOMContentLoaded', () => {
          kittyItems.forEach((item) => {
             item.parentNode.removeChild(item);
          })
-     }, 5000)
+     }, 8000)
          
     const catGif = document.createElement("img");
     catGif.id = "picture";
     catGif.src = "https://media3.giphy.com/media/CjmvTCZf2U3p09Cn0h/giphy.gif?cid=790b76115cf02c073650366355eafb88&rid=giphy.gif";
     catGif.style.cssFloat = "right"
-    document.querySelector("h1").appendChild(catGif)
+    catGif.style.width = "100%";
+    document.querySelector(".div3").prepend(catGif)
 
     // // add css
     var myCss = document.createElement("link");
@@ -40,6 +42,30 @@ document.addEventListener('DOMContentLoaded', () => {
     myCss.rel = "stylesheet";
     myCss.type = 'text/css'; 
     document.head.appendChild(myCss);
+
+    // screen flicker
+    setInterval(() => {
+        document.querySelectorAll("li").forEach((item) => {
+            // item.style.opacity = (generateNumber()) / 12;
+            var rect = item.getBoundingClientRect();
+            item.style.opacity = ((window.innerHeight - rect.top)-50)/window.innerHeight;
+        })
+        // document.getElementById("salmon").style.color === "blue" ? 
+        //     document.getElementById("salmon").style.color = "red" :
+        //     document.getElementById("salmon").style.color = "blue";
+    }, 80)
+
+
+    const textColor = "lightgreen";
+    const shadowColor = "#ccff00";
+    const backgroundColor = "#010e04f0";
+
+
+    // add new colors
+    document.body.style.backgroundColor = "yellow";
+    document.body.style.color = textColor;
+    // document.getElementById("salmon").style.color = "red";
+    document.body.style.textShadow = `0px 3px 12px ${shadowColor}`;
 
 })
 
@@ -52,9 +78,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // //(1..12).to_a.sample
 
-// function generateNumber() {
-//     return Math.ceil(Math.random() * 12);
-// }
+function generateNumber() {
+    return Math.ceil(Math.random() * 12);
+}
 
 // //random hex color
 // function randomColor() {
@@ -63,9 +89,6 @@ document.addEventListener('DOMContentLoaded', () => {
 //     }).join('');
 // }
 
-// const textColor = "lightgreen";
-// const shadowColor = "#ccff00";
-// const backgroundColor = "#010e04f0";
 // // const textColor = randomColor();
 // // const shadowColor = randomColor();
 // // const backgroundColor = randomColor();
@@ -109,11 +132,7 @@ document.addEventListener('DOMContentLoaded', () => {
 //     },3000)
 
 
-// // add new colors
-//     document.body.style.backgroundColor = backgroundColor;
-//     document.body.style.color = textColor;
-//     // document.getElementById("salmon").style.color = "red";
-//     document.body.style.textShadow = `0px 3px 12px ${shadowColor}`;
+
 
 // // add image
 //     const catGif = document.createElement("img");
